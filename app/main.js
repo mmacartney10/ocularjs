7e7b92627a4771bda324d46ed7dd7f1a10f848cc
matthewmacartney <matthew.macartney@codecomputerlove.com>
2016-08-27 14:39:52 +0100

    Refactor: updated process so function names are improved and childProcess output is easier to read, updated main so that action is dispatched in own function

--- app/main.js
+++ app/main.js
@@ -1,31 +1,30 @@
 module.exports = function (currentDirectory) {
 
-  const store = require('./store')
+  const store = require('./store');
   const process = require('./process')(currentDirectory);
-  const inquirer = require('inquirer')
-  const fs = require('fs')
+  const inquirer = require('inquirer');
+  const fs = require('fs');
 
-  var questions = [
-    {
-      type: 'list',
-      name: 'openingQuestion',
-      message: 'Would you like to run reference or test screenshots?:',
-      choices: [ 'Reference', 'Test', 'Exit' ],
-      default: 'Reference'
-    }
-  ]
+  var questions = [{
+    type: 'list',
+    name: 'screenShotType',
+    message: 'Would you like to run Reference or Test screenshots?',
+    choices: [ 'Reference', 'Test', 'Exit' ],
+    default: 'Reference'
+  }];
 
   function askQuestion () {
     inquirer.prompt(questions).then(function (answers) {
-      if (answers.openingQuestion === 'Exit') return;
+      if (answers.screenShotType === 'Exit') return;
+      sendQuestionResultToStore(answers.screenShotType);
+    });
+  }
 
-
-
-      store.dispatch({
-        type: 'APP',
-        answer: answers.openingQuestion,
-        completed: false
-      });
+  function sendQuestionResultToStore (answer) {
+    store.dispatch({
+      type: 'APP',
+      answer: answer,
+      completed: false
     });
   }
 
@@ -37,5 +36,5 @@ module.exports = function (currentDirectory) {
 
   askQuestion();
 
-  store.subscribe(handleChange)
+  store.subscribe(handleChange);
 }
--- app/process.js
+++ app/process.js
@@ -3,10 +3,9 @@ module.exports = function (currentDirectory) {
   const store = require('./store')
 
   var path = require('path');
-  var phantomjs = require('phantomjs');
-  var spawn = require('child_process').spawn;
-
   var colors = require('colors');
+  var phantomjs = require('phantomjs');
+  var spawn = require('child_process').spawn
 
   const fileName = 'child-process.js';
   const filePath = path.join(__dirname, fileName);
@@ -17,15 +16,15 @@ module.exports = function (currentDirectory) {
   var viewportLength = Object.keys(data.viewports).length;
   var currentViewportIndex = 0;
 
-  function runChildProcess () {
+  function loopThroughViewports () {
     currentViewportIndex = 0;
 
     for (var viewport in data.viewports) {
-      createScreenShots(viewport, data.viewports[viewport]);
+      runChildProcess(viewport, data.viewports[viewport]);
     }
   }
 
-  function createScreenShots (viewportName, viewportDimensions) {
+  function runChildProcess (viewportName, viewportDimensions) {
 
     var childArgs = [
       filePath,
@@ -39,49 +38,50 @@ module.exports = function (currentDirectory) {
     var child = spawn(phantomjs.path, childArgs);
 
     child.stdout.on('data', (data) => {
-      var output = `stdout: ${data}`;
-      var answer = store.getState()[0].answer;
-
-      if (answer === 'Reference') {
-        console.log(output.yellow);
-        return;
-      }
-
-      if (output.indexOf('Passed') > -1) {
-        console.log(output.green);
-        return;
-      }
-
-      console.log(output.red);
-
+      logOutputWithColour(`* ${data}`);
     });
 
     child.stderr.on('data', (data) => {
-      var output = `stderr: ${data}`;
-      console.log(output.red);
+      console.log(`stderr: ${data}`.red);
     });
 
     child.on('exit', (code) => {
+      dispatchActionWhenLastChildProcessEnds();
+    });
+  }
 
-      currentViewportIndex += 1;
+  function logOutputWithColour (output) {
+    var answer = store.getState()[0].answer;
 
-      if (currentViewportIndex === viewportLength) {
-        store.dispatch({
-          type: 'APP',
-          answer: store.getState()[0].answer,
-          completed: true
-        });
-      }
+    if (answer === 'Reference') {
+      console.log(output.yellow);
+      return;
+    }
+
+    if (output.indexOf('Passed') > -1) {
+      console.log(output.green);
+      return;
+    }
+
+    console.log(output.red);
+  }
+
+  function dispatchActionWhenLastChildProcessEnds () {
+    currentViewportIndex += 1;
+    if (currentViewportIndex !== viewportLength) return;
+
+    store.dispatch({
+      type: 'APP',
+      answer: store.getState()[0].answer,
+      completed: true
     });
   }
 
   function handleChange () {
     var completed = store.getState()[0].completed;
-
     if (completed) return;
-
-    runChildProcess();
+    loopThroughViewports();
   }
 
-  store.subscribe(handleChange)
+  store.subscribe(handleChange);
 }
